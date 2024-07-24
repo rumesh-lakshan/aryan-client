6b1a3df512122757cdecadc6bc4c61eb8e5e075e
PeirisWRL <123114138+rumesh-lakshan@users.noreply.github.com>
2024-07-24 21:49:22 +0530

    Pasindu Fernando

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,45 +1,41 @@
 import { message } from "antd";
 import axios from "axios";
 
-export const userLogin = (reqObj, navigate) => async (dispatch) => {
+export const userLogin = (reqObj) => async (dispatch) => {
   dispatch({ type: "LOADING", payload: true });
+
+  // const user = localStorage.getItem("user");
+  // const username = user.username;
 
   try {
     const response = await axios.post("/api/users/login", reqObj);
     localStorage.setItem("user", JSON.stringify(response.data));
-    message.success("Login Successful");
-    dispatch({ type: "LOADING", payload: false });
+    message.success("Login Successfull");
     setTimeout(() => {
-      navigate("/");
+      window.location.href = "/";
     }, 500);
+    dispatch({ type: "LOADING", payload: false });
   } catch (error) {
-    console.error(error);
-    if (error.response && error.response.data.message) {
-      message.error(error.response.data.message);
-    } else {
-      message.error("Something went wrong. Please try again.");
-    }
+    console.log(error);
+    error.response.data.message && message.error(error.response.data.message);
     dispatch({ type: "LOADING", payload: false });
   }
 };
 
-export const userRegister = (reqObj, navigate) => async (dispatch) => {
+export const userRegister = (reqObj) => async (dispatch) => {
   dispatch({ type: "LOADING", payload: true });
 
   try {
-    await axios.post("/api/users/register", reqObj);
-    message.success("Registration Successful");
-    dispatch({ type: "LOADING", payload: false });
+    const response = await axios.post("/api/users/register", reqObj);
+    message.success("Registeration Successfull");
     setTimeout(() => {
-      navigate("/login");
+      window.location.href = "/login";
     }, 500);
+
+    dispatch({ type: "LOADING", payload: false });
   } catch (error) {
-    console.error(error);
-    if (error.response && error.response.data.message) {
-      message.error(error.response.data.message);
-    } else {
-      message.error("Something went wrong. Please try again.");
-    }
+    console.log(error);
+    error.response.data.message && message.error(error.response.data.message);
     dispatch({ type: "LOADING", payload: false });
   }
 };

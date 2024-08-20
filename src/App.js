b5922aeb8033db0d1dfca4c74a75c0f78196659e
PeirisWRL <123114138+rumesh-lakshan@users.noreply.github.com>
2024-08-20 20:33:52 +0530

    Pasindu Fernando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,41 +12,47 @@ import EditCar from "./pages/EditCar";
 import AllBookings from "./pages/AllBookings";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://aryan-api.onrender.com";
-
 function App() {
+  axios.defaults.baseURL = "https://aryan-api.onrender.com";
   const user = JSON.parse(localStorage.getItem("user"));
+
+  const AdminRoutes = () => (
+    <Routes>
+      <Route path="/" element={<Navigate to="/admin" />} />
+      <Route path="/admin" element={<AdminHome />} />
+      <Route path="/addcar" element={<AddCar />} />
+      <Route path="/editcar/:carid" element={<EditCar />} />
+      <Route path="/allbookings" element={<AllBookings />} />
+    </Routes>
+  );
+
+  const UserRoutes = () => (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/bookcar/:carid" element={<BookingCar />} />
+      <Route path="/userbookings" element={<UserBookings />} />
+    </Routes>
+  );
+
+  const PublicRoutes = () => (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          {user ? (
-            user.role === "Admin" ? (
-              <>
-                <Route path="/" element={<Navigate to="/admin" />} />
-                <Route path="/admin" element={<AdminHome />} />
-                <Route path="/addcar" element={<AddCar />} />
-                <Route path="/editcar/:carid" element={<EditCar />} />
-                <Route path="/allbookings" element={<AllBookings />} />
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<Navigate to="/home" />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/bookcar/:carid" element={<BookingCar />} />
-                <Route path="/userbookings" element={<UserBookings />} />
-              </>
-            )
-          ) : (
-            <>
-              <Route path="/" element={<Navigate to="/login" />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </>
-          )}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        {user && user.role === "Admin" ? (
+          <AdminRoutes />
+        ) : user && user.role === "User" ? (
+          <UserRoutes />
+        ) : (
+          <PublicRoutes />
+        )}
       </BrowserRouter>
     </div>
   );
